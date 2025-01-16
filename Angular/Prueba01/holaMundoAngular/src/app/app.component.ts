cbb4e970d6f69f9d233c85434725eb58b3cf3fa2
raulrp15 <raulromerapavon@gmail.com>
2025-01-16 14:33:29 +0100

    cascajo

--- Angular/Prueba01/holaMundoAngular/src/app/app.component.ts
+++ Angular/Prueba01/holaMundoAngular/src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TablaPersonasComponent } from './components/tabla-personas/tabla-personas.component';
+import { FormulariopersonaComponent } from './components/formulariopersona/formulariopersona.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TablaPersonasComponent, FormulariopersonaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'holaMundoAngular';
 }
---
+++ Angular/Prueba01/holaMundoAngular/src/app/components/tabla-personas/tabla-personas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-personas',
+  imports: [],
+  templateUrl: './tabla-personas.component.html',
+  styleUrl: './tabla-personas.component.css'
+})
+export class TablaPersonasComponent {
+
+}
