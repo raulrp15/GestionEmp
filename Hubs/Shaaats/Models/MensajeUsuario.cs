5b1613dd35c7d4692b228fcd45b264011c4ed682
raulrp15 <raulromerapavon@gmail.com>
2025-01-20 14:19:58 +0100

    marikilla

---
+++ Hubs/Shaaats/Models/MensajeUsuario.cs
@@ -0,0 +1,8 @@
+﻿namespace Models
+{
+    public class MensajeUsuario
+    {
+        public string Usuario {  get; set; }
+        public string Mensaje { get; set; }
+    }
+}
--- Hubs/Shaaats/NewSHAT/NewSHAT/Hubs/ChatHub.cs
+++ Hubs/Shaaats/NewSHAT/NewSHAT/Hubs/ChatHub.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Models;
 namespace NewSHAT.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(MensajeUsuario mensajeUsuario)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", mensajeUsuario);
         }
     }
 }
